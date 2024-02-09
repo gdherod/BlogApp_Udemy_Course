0bc1dae8d5f6295f154498f667b973f387f5cb58
Gary Hermosilla Rodríguez <garyhermosilla@gmail.com>
2024-02-09 16:30:53 -0300

    fix: hide bottom navigation on some screens

--- app/src/main/java/com/example/blogapp/MainActivity.kt
+++ app/src/main/java/com/example/blogapp/MainActivity.kt
@@ -2,20 +2,39 @@ package com.example.blogapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
+import com.example.blogapp.core.hide
+import com.example.blogapp.core.show
 import com.example.blogapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
+        navController = navHostFragment.navController
         binding.bottomNavigationView.setupWithNavController(navController)
+        observeDestinationChange()
+    }
+
+    private fun observeDestinationChange() {
+        navController.addOnDestinationChangedListener { controller, destination, arguments ->
+            when (destination.id) {
+                R.id.loginFragment, R.id.registerFragment -> {
+                    binding.bottomNavigationView.hide()
+                }
+
+                else -> {
+                    binding.bottomNavigationView.show()
+                }
+            }
+        }
     }
 }
