bd2d02b44149e6915ebaff4b872085d477c6e40f
Gary Hermosilla Rodríguez <garyhermosilla@gmail.com>
2024-02-09 16:30:53 -0300

    feat: take picture function and layout

--- app/src/main/java/com/example/blogapp/ui/camera/CameraFragment.kt
+++ app/src/main/java/com/example/blogapp/ui/camera/CameraFragment.kt
@@ -1,8 +1,42 @@
 package com.example.blogapp.ui.camera
 
+import android.app.Activity.RESULT_OK
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.graphics.Bitmap
+import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.blogapp.R
+import com.example.blogapp.databinding.FragmentCameraBinding
 
 class CameraFragment : Fragment(R.layout.fragment_camera) {
 
+    private val REQUEST_IMAGE_CAPTURE = 1
+    private lateinit var binding: FragmentCameraBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentCameraBinding.bind(view)
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        try {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+        } catch (e: ActivityNotFoundException) {
+            Toast.makeText(
+                requireContext(),
+                "No se encontro ninguna app para abrir la camara.",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_IMAGE_CAPTURE && requestCode == RESULT_OK) {
+            val imageBitmap = data?.extras?.get("data") as Bitmap
+            binding.imgAddPhoto.setImageBitmap(imageBitmap)
+        }
+    }
 }
