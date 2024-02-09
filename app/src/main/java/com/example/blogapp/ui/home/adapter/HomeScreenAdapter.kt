1c3619cd3fdfd5d62fd0c484e32101c66a510070
Gary Hermosilla Rodríguez <garyhermosilla@gmail.com>
2024-02-09 16:30:52 -0300

    feat: added HomeScreenAdapter

---
+++ app/src/main/java/com/example/blogapp/ui/home/adapter/HomeScreenAdapter.kt
@@ -0,0 +1,40 @@
+package com.example.blogapp.ui.home.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.blogapp.core.BaseViewHolder
+import com.example.blogapp.data.model.Post
+import com.example.blogapp.databinding.PostItemViewBinding
+
+class HomeScreenAdapter(private val postList: List<Post>) :
+    RecyclerView.Adapter<BaseViewHolder<*>>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder<*> {
+        val itemBinding =
+            PostItemViewBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return HomeScreenViewHolder(itemBinding, parent.context)
+    }
+
+    override fun getItemCount(): Int = postList.size
+
+    override fun onBindViewHolder(holder: BaseViewHolder<*>, position: Int) {
+        when (holder) {
+            is HomeScreenViewHolder -> holder.bind(postList[position])
+        }
+    }
+
+    private inner class HomeScreenViewHolder(
+        val binding: PostItemViewBinding,
+        val context: Context
+    ) : BaseViewHolder<Post>(binding.root) {
+        override fun bind(item: Post) {
+            Glide.with(context).load(item.postImage).centerCrop().into(binding.postImage)
+            Glide.with(context).load(item.profilePicture).centerCrop().into(binding.profilePicture)
+            binding.profileName.text = item.profileName
+            binding.postTimestamp.text = "Hace 2 horas"
+        }
+
+    }
+}
