d7ef58fba76987732bd13f4a3a73a295e3e8e52a
Gary Hermosilla Rodríguez <garyhermosilla@gmail.com>
2024-02-09 16:30:52 -0300

    chore: move package

--- app/src/main/java/com/example/blogapp/presentation/main/HomeScreenViewModel.kt
+++ app/src/main/java/com/example/blogapp/presentation/main/HomeScreenViewModel.kt
@@ -1,10 +1,10 @@
-package com.example.blogapp.presentation
+package com.example.blogapp.presentation.main
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.liveData
 import com.example.blogapp.core.Resource
-import com.example.blogapp.domain.HomeScreenRepo
+import com.example.blogapp.domain.home.HomeScreenRepo
 import kotlinx.coroutines.Dispatchers
 
 class HomeScreenViewModel(private val repo: HomeScreenRepo) : ViewModel() {
