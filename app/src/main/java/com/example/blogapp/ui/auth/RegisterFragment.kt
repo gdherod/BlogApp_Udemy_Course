7a7cf0096132ff88c4ea2fb4cfd557d36c64ce82
Gary Hermosilla Rodríguez <garyhermosilla@gmail.com>
2024-02-09 16:30:53 -0300

    feat: added create user function

--- app/src/main/java/com/example/blogapp/ui/auth/RegisterFragment.kt
+++ app/src/main/java/com/example/blogapp/ui/auth/RegisterFragment.kt
@@ -1,23 +1,38 @@
 package com.example.blogapp.ui.auth
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
 import com.example.blogapp.R
+import com.example.blogapp.core.Result
+import com.example.blogapp.data.remote.auth.AuthDataSource
 import com.example.blogapp.databinding.FragmentRegisterBinding
+import com.example.blogapp.domain.auth.AuthRepoImpl
+import com.example.blogapp.presentation.auth.AuthViewModel
+import com.example.blogapp.presentation.auth.AuthViewModelFactory
 
 class RegisterFragment : Fragment(R.layout.fragment_register) {
 
     private lateinit var binding: FragmentRegisterBinding
+    private val viewModel by viewModels<AuthViewModel> {
+        AuthViewModelFactory(
+            AuthRepoImpl(
+                AuthDataSource()
+            )
+        )
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentRegisterBinding.bind(view)
-        SignUp()
+        signUp()
     }
 
-    private fun SignUp() {
+    private fun signUp() {
         binding.btnSignup.setOnClickListener {
             val username = binding.editTextUserName.text.toString().trim()
             val email = binding.editTextEmail.text.toString().trim()
@@ -32,8 +47,34 @@ class RegisterFragment : Fragment(R.layout.fragment_register) {
                 )
             ) return@setOnClickListener
 
-            Log.d("signUpDate", "data: $username $email $password $confirmPassword")
+            createUser(username, email, password)
         }
+    }
+
+    private fun createUser(username: String, email: String, password: String) {
+        viewModel.signUp(username, email, password).observe(viewLifecycleOwner, Observer { result ->
+            when (result) {
+                is Result.Loading -> {
+                    binding.progressBar.visibility = View.VISIBLE
+                    binding.btnSignup.isEnabled = false
+                }
+
+                is Result.Success -> {
+                    binding.progressBar.visibility = View.GONE
+                    findNavController().navigate(R.id.action_registerFragment_to_homeScreenFragment)
+                }
+
+                is Result.Failure -> {
+                    binding.progressBar.visibility = View.GONE
+                    binding.btnSignup.isEnabled = true
+                    Toast.makeText(
+                        requireContext(),
+                        "Error: ${result.exception}",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+        })
     }
 
     private fun validateUserData(
