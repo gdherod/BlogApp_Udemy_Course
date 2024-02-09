4b289a1e87e3a48387c416098f324c7543d8ab7f
Gary Hermosilla Rodríguez <garyhermosilla@gmail.com>
2024-02-09 16:30:52 -0300

    feat: added layout components

---
+++ app/src/main/java/com/example/blogapp/ui/home/HomeScreenFragment.kt
@@ -0,0 +1,15 @@
+package com.example.blogapp.ui.home
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.example.blogapp.R
+
+class HomeScreenFragment : Fragment(R.layout.fragment_home_screen) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+}
