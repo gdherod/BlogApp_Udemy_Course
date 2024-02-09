69f3c76aacce5c563038017dff17e9b30d0e9071
Gary Hermosilla Rodríguez <garyhermosilla@gmail.com>
2024-02-09 16:30:52 -0300

    feat: data layer

---
+++ app/src/main/java/com/example/blogapp/domain/HomeScreenRepoImpl.kt
@@ -0,0 +1,9 @@
+package com.example.blogapp.domain
+
+import com.example.blogapp.core.Resource
+import com.example.blogapp.data.model.Post
+import com.example.blogapp.data.remote.HomeScreenDataSource
+
+class HomeScreenRepoImpl(private val dataSource: HomeScreenDataSource): HomeScreenRepo {
+    override suspend fun getLatestPost(): Resource<List<Post>> = dataSource.getLatestPost()
+}
