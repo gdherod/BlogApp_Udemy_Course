304297663442729c90ee400cf1e297fb17df430a
Gary Hermosilla Rodríguez <garyhermosilla@gmail.com>
2024-02-09 16:30:52 -0300

    feat: added model

---
+++ app/src/main/java/com/example/blogapp/data/model/Post.kt
@@ -0,0 +1,10 @@
+package com.example.blogapp.data.model
+
+import com.google.firebase.Timestamp
+
+data class Post(
+    val profilePicture: String = "",
+    val profileName: String = "",
+    val postTimestamp: Timestamp? = null,
+    val postImage: String = ""
+)
