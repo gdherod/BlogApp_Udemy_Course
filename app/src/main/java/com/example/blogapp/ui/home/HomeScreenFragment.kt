aec8df09fac8f0a6e1d759f8d27486314b68a9f9
Gary Hermosilla Rodríguez <garyhermosilla@gmail.com>
2024-02-09 16:30:52 -0300

    feat: showing hardcode data

--- app/src/main/java/com/example/blogapp/ui/home/HomeScreenFragment.kt
+++ app/src/main/java/com/example/blogapp/ui/home/HomeScreenFragment.kt
@@ -4,12 +4,26 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import com.example.blogapp.R
+import com.example.blogapp.data.model.Post
+import com.example.blogapp.databinding.FragmentHomeScreenBinding
+import com.example.blogapp.ui.home.adapter.HomeScreenAdapter
+import com.google.firebase.Timestamp
 
 class HomeScreenFragment : Fragment(R.layout.fragment_home_screen) {
 
+    private lateinit var binding: FragmentHomeScreenBinding
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding = FragmentHomeScreenBinding.bind(view)
 
+        val postList = listOf(
+            Post("profile", "User 1", Timestamp.now(), "urlImage"),
+            Post("profile", "User 2", Timestamp.now(), "urlImage"),
+            Post("profile", "User 3", Timestamp.now(), "urlImage")
+        )
+
+        binding.rvHome.adapter = HomeScreenAdapter(postList)
     }
 
 }
