376a649e69d722eec331232fca0de02e02cae8be
Gary Hermosilla Rodríguez <garyhermosilla@gmail.com>
2024-02-09 16:30:53 -0300

    feat: added navigation menu

---
+++ app/src/main/java/com/example/blogapp/ui/camera/CameraFragment.kt
@@ -0,0 +1,8 @@
+package com.example.blogapp.ui.camera
+
+import androidx.fragment.app.Fragment
+import com.example.blogapp.R
+
+class CameraFragment : Fragment(R.layout.fragment_camera) {
+
+}
---
+++ app/src/main/java/com/example/blogapp/ui/profile/ProfileFragment.kt
@@ -0,0 +1,8 @@
+package com.example.blogapp.ui.profile
+
+import androidx.fragment.app.Fragment
+import com.example.blogapp.R
+
+class ProfileFragment : Fragment(R.layout.fragment_profile) {
+
+}
