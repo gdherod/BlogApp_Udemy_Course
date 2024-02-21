b2ab8b38470625c8b5aec838a5ce3888a4341a5d
Gary Hermosilla Rodríguez <garyhermosilla@gmail.com>
2024-02-21 17:56:09 -0300

    feat: changed to order post by created at

--- app/src/main/java/com/example/blogapp/data/remote/home/HomeScreenDataSource.kt
+++ app/src/main/java/com/example/blogapp/data/remote/home/HomeScreenDataSource.kt
@@ -4,13 +4,15 @@ import com.example.blogapp.core.Result
 import com.example.blogapp.data.model.Post
 import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
 import kotlinx.coroutines.tasks.await
 
 class HomeScreenDataSource {
 
     suspend fun getLatestPost(): Result<List<Post>> {
         val postList = mutableListOf<Post>()
-        val querySnapshot = FirebaseFirestore.getInstance().collection("posts").get().await()
+        val querySnapshot = FirebaseFirestore.getInstance().collection("posts")
+            .orderBy("createdAt", Query.Direction.DESCENDING).get().await()
         for (post in querySnapshot.documents) {
             post.toObject(Post::class.java)?.let { fbPost ->
                 fbPost.apply {
