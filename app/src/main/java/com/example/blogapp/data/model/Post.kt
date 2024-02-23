f0d63adf6f2f9e521016073fd66a7114c07bad8f
Gary Hermosilla Rodríguez <garyhermosilla@gmail.com>
2024-02-23 18:58:48 -0300

    feat: edit model for like post

--- app/src/main/java/com/example/blogapp/data/model/Post.kt
+++ app/src/main/java/com/example/blogapp/data/model/Post.kt
@@ -1,14 +1,24 @@
 package com.example.blogapp.data.model
 
+import com.google.firebase.firestore.Exclude
 import com.google.firebase.firestore.ServerTimestamp
 import java.util.Date
 
 data class Post(
-    val profilePicture: String = "",
-    val profileName: String = "",
+    @Exclude @JvmField
+    val id: String = "",
     @ServerTimestamp
     var createdAt: Date? = null,
     val postImage: String = "",
     val postDescription: String = "",
-    val uid: String = ""
+    val poster: Poster? = null,
+    val likes: Long = 0,
+    @Exclude @JvmField
+    var liked: Boolean = false
+)
+
+data class Poster(
+    val username: String? = "",
+    val profilePicture: String = "",
+    val uid: String? = null
 )
