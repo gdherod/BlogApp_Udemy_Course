1ea6ae9b2a2e63e8f3959faafd3d1f70bcd2ede0
Gary Hermosilla Rodríguez <garyhermosilla@gmail.com>
2024-02-23 17:27:47 -0300

    feat: flow for realtime posts

--- app/src/main/java/com/example/blogapp/domain/home/HomeScreenRepoImpl.kt
+++ app/src/main/java/com/example/blogapp/domain/home/HomeScreenRepoImpl.kt
@@ -3,7 +3,8 @@ package com.example.blogapp.domain.home
 import com.example.blogapp.core.Result
 import com.example.blogapp.data.model.Post
 import com.example.blogapp.data.remote.home.HomeScreenDataSource
+import kotlinx.coroutines.flow.Flow
 
-class HomeScreenRepoImpl(private val dataSource: HomeScreenDataSource): HomeScreenRepo {
-    override suspend fun getLatestPost(): Result<List<Post>> = dataSource.getLatestPost()
+class HomeScreenRepoImpl(private val dataSource: HomeScreenDataSource) : HomeScreenRepo {
+    override suspend fun getLatestPost(): Flow<Result<List<Post>>> = dataSource.getLatestPost()
 }
--- app/src/main/java/com/example/blogapp/presentation/main/HomeScreenViewModel.kt
+++ app/src/main/java/com/example/blogapp/presentation/main/HomeScreenViewModel.kt
@@ -3,23 +3,28 @@ package com.example.blogapp.presentation.main
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.liveData
+import androidx.lifecycle.viewModelScope
 import com.example.blogapp.core.Result
 import com.example.blogapp.domain.home.HomeScreenRepo
 import kotlinx.coroutines.Dispatchers
 
 class HomeScreenViewModel(private val repo: HomeScreenRepo) : ViewModel() {
 
-    fun fetchLatestPost() = liveData(Dispatchers.IO) {
+    fun fetchLatestPost() = liveData(viewModelScope.coroutineContext + Dispatchers.Main) {
         emit(Result.Loading())
-        try {
-            emit(repo.getLatestPost())
-        } catch (e: Exception) {
-            emit(Result.Failure(e))
+        runCatching {
+            repo.getLatestPost()
+        }.onSuccess { flowList ->
+            flowList.collect {
+                emit(it)
+            }
+        }.onFailure { throwable ->
+            emit(Result.Failure(Exception(throwable.message)))
         }
     }
 }
 
-class HomeScreenViewModelFactory(private val repo: HomeScreenRepo): ViewModelProvider.Factory {
+class HomeScreenViewModelFactory(private val repo: HomeScreenRepo) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         return modelClass.getConstructor(HomeScreenRepo::class.java).newInstance(repo)
     }
